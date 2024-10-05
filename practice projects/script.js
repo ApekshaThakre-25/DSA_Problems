a40adc9885daead41469d00a2135d0b6190d5fee
ApekshaThakre-25 <apekshadipak.thakre@adixoo.com>
2024-10-05 06:40:52 -0700

    made changes in programs

--- practice projects/script.js
+++ practice projects/script.js
@@ -394,4 +394,87 @@ function merge(left, right) {
 
 console.log(mergeSort([38, 27, 43, 3, 9, 82, 10]));
 
+function heapify(arr, n, i) {
+  let largest = i;
+  let left = 2 * i + 1;
+  let right = 2 * i + 2;
+
+  if (left < n && arr[left] > arr[largest]) {
+    largest = left;
+  }
+
+  if (right < n && arr[right] > arr[largest]) {
+    largest = right;
+  }
+
+  if (largest !== i) {
+    [arr[i], arr[largest]] = [arr[largest], arr[i]];
+    heapify(arr, n, largest);
+  }
+}
+
+function heapSort(arr) {
+  const n = arr.length;
+
+  for (let i = Math.floor(n / 2) - 1; i >= 0; i--) {
+    heapify(arr, n, i);
+  }
+
+  for (let i = n - 1; i > 0; i--) {
+    [arr[0], arr[i]] = [arr[i], arr[0]];
+    heapify(arr, i, 0);
+  }
+
+  return arr;
+}
+
+console.log(heapSort([12, 11, 13, 5, 6, 7]));
+
+function quickSort(arr) {
+  if (arr.length <= 1) {
+    return arr;
+  }
+
+  const pivot = arr[arr.length - 1];
+  const left = [];
+  const right = [];
+
+  for (let i = 0; i < arr.length - 1; i++) {
+    if (arr[i] < pivot) {
+      left.push(arr[i]);
+    } else {
+      right.push(arr[i]);
+    }
+  }
+
+  return [...quickSort(left), pivot, ...quickSort(right)];
+}
+
+console.log(quickSort([12, 11, 13, 5, 6, 7]));
+
+
+function selectionSort(arr) {
+  const n = arr.length;
+
+  for (let i = 0; i < n - 1; i++) {
+        let minIndex = i;
+
+     
+      for (let j = i + 1; j < n; j++) {
+          if (arr[j] < arr[minIndex]) { 
+              minIndex = j; 
+          }
+      }
+
+      
+      if (minIndex !== i) {
+          [arr[i], arr[minIndex]] = [arr[minIndex], arr[i]];
+      }
+  }
+
+  return arr;
+}
+
+
+console.log(selectionSort( [64, 25, 12, 22, 11]));
 
